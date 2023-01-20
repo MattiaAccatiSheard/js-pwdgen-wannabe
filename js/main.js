43b1c9ab630523baad8aa3504edc356fd0dd3095
MattiaAccatiSheard <mattiahtsheard@gmail.com>
2023-01-20 16:02:07 +0100

    .prova.console.e.prompt

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//chiedo qualcosa di prova con prompt
+//struttura: prefix var - nome var - valore var
+//let username = "Mattia";
+
+
+//lo rendo visibile  con console.log(nome.var)
+//funziona, ma (da errore:Uncaught TypeError: Cannot read properties of null (reading 'getContext') at screen.js: 1: 754 at screen.js: 1: 4630)
+
+
+//non credo sia importante : PROVO PROMPT
+let username = prompt('come ti chiami?');
+console.log(username);
+// subito non funzionava: REGOLA POSSIBILE:
+//DEVO METTERE CONSOLE DOPO LA VARIABILE CHE LA DESCRIVE E MAI PRIMA
